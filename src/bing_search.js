e3f5e4aef762decc262f92ce9923a73520744b45
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2016-11-16 13:32:33 +0100

    Changed first param of search() to object.

--- src/bing_search.js
+++ src/bing_search.js
@@ -1,21 +1,26 @@
 
 const Search = require('bing.search');
 
+/** A wrapper around bing search module. */
 var BingSearch = function(key) {
-    
+
     var key = key;
     var search = new Search(key);
-    
-    this.search = function(expr, offset) {
+
+    this.search = function(query, func) {
+        var expr = query.keyword;
+        var offset = query.offset;
         search.images(expr,
             {top: 10, skip: offset},
-            function(err, results) {
-                if (err) throw err;
-                return results;
+            (err, results) => {
+                if (err) func(err);
+                else {
+                    func(null, results);
+                }
             }
         );
     };
-    
+
 };
 
 module.exports = BingSearch;
